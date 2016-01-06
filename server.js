5c0f2281e0087cfe9cfba3049ee9089580321252
Rick Bacci <rickbacci@gmail.com>
2016-01-05 18:49:41 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express    = require('express');
+const path       = require('path');
+const bodyParser = require('body-parser');
+const generateId = require('./lib/generate-id');
+const app        = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
+
+app.set('view engine', 'jade')
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Express';
+app.locals.pizza = {};
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.post('/pizzas', (request, response) => {
+  console.log(request.body)
+  if (!request.body.pizza) { return response.sendStatus(400); }
+
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body.pizza;
+
+  response.redirect('/pizzas/' + id);
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
+
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+
+module.exports = app;
